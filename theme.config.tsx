97ee550f34345ae01b1357dccbcb629c5453f6ce
skyh24 <skyhighfeng@gmail.com>
2024-05-12 23:50:32 +0800

    Doc: summary

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,17 +1,28 @@
 import React from 'react'
+import Image from "next/image";
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  
+  logo: <div className="flex-shrink items-center justify-center gap-2.5 hover:cursor-pointer">
+          <Image
+            src="/logo/ramen-logo.svg"
+            alt="Ramen Logo"
+            width={26.86}
+            height={23.55}
+            loading="eager"
+          />
+          <h1 className="text-white text-xl font-medium">RamenSwap</h1>
+        </div>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/orgs/chef-studio/repositories',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://t.me/ramenswap.xyz',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/chef-studio/ramen-docs',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'RamenSwap © 2024',
   },
 }
 
